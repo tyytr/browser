0b0396172e66e07a0de5999a4bd125e3194b808c
tyytr <33309352+tyytr@users.noreply.github.com>
2017-11-14 17:13:18 +0800

    Add files via upload

--- demo/js/test.js
+++ demo/js/test.js
@@ -1,7 +1,13 @@
 // JavaScript Document
 $(document).ready(function(){
 	$("#navBar .dropdown-menu a").click(function(){
-			var href= $( this ).attr("href");
-			$("#tab-list a[href='"+href+"']").tab("show");
-		});
+		var href= $( this ).attr("href");
+		$("#tab-list a[href='"+href+"']").tab("show");
+	});
+	// $("#navBar .nav li a").click(function() {
+	// 	/* Act on the event */
+	// 	var name= $( this ).attr("href");
+	// 	$("div[id='"+name+"']").tab("show");
+	// });
+	
 });
